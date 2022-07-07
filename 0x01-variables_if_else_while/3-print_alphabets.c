e57a535bf47266f8c02d8820893aa0dd4b154f5b
Roserotash <roserotash@gmail.com>
2022-07-07 05:30:46 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always0 (success)
+ */
+int main(void)
+{
+char abc;
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+putchar(abc);
+}
+putchar('\n');
+return (0);
+}
+
